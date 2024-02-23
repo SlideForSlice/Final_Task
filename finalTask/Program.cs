c31c7ddfcbfe6499aac072b5c9526d602e4f230f
Lil master <kenigilya9@gmail.com>
2024-02-23 23:03:07 +0500

    added logic

--- finalTask/Program.cs
+++ finalTask/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"1234", "bob", "shakira"};
+int countForLength = 0;
+int countForJ = 0;
+
+for (int i = 0; i < array.Length; i++){
+    int sumOfChars = array[i].Length;
+    if (sumOfChars <= 3){
+        countForLength += 1;
+    }
+}
+
+string[] newArray = new string[countForLength];
+
+for (int i = 0; i < array.Length; i++){
+    int sumOfChars = array[i].Length;
+    if (sumOfChars <= 3){
+        newArray[countForJ] = array[i];
+        countForJ += 1;
+    }
+}
+
+Console.WriteLine(newArray);
